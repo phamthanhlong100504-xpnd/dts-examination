86873cd5bb8bc015303ff37147570257061c1cc8
ducvui251 <vuivd.b22cn921@gmail.com>
2026-08-28 10:34:52 +0700

    feat(kafka): add learning-results topic definition (3 partitions)

--- src/main/java/dts/com/examination/ExaminationApplication.java
+++ src/main/java/dts/com/examination/ExaminationApplication.java
@@ -1,15 +1,25 @@
 package dts.com.examination;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ExaminationApplication {
+
+	@Bean
+	public NewTopic learningResultsTopic() {
+		return TopicBuilder.name("learning-results")
+				.partitions(3)
+				.replicas(1)
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExaminationApplication.class, args);
